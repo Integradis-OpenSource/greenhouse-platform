64272e144ccf5b13a4a5b461246051ec1b1d5dba
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-10-18 12:38:40 -0500

    chore: Initial commit

---
+++ src/main/java/com/acme/greenhouse/platform/GreenhousePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.greenhouse.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhousePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenhousePlatformApplication.class, args);
+    }
+
+}
