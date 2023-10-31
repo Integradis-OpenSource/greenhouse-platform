357ff1df8b025b06dff343ea1d661df14d1daeb5
CarloLSG <u20211a475@upc.edu.pe>
2023-10-31 12:27:17 -0500

    chore(crops): entity for each phase of the crop has its necessary constructor.

--- src/main/java/com/integradis/greenhouse/platform/crops/domain/model/entities/Bunker.java
+++ src/main/java/com/integradis/greenhouse/platform/crops/domain/model/entities/Bunker.java
@@ -1,36 +1,39 @@
 package com.integradis.greenhouse.platform.crops.domain.model.entities;
 
 import com.integradis.greenhouse.platform.crops.domain.model.aggregates.Crop;
+import com.integradis.greenhouse.platform.crops.domain.model.valueobjects.CropPhase;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-import java.util.Date;
 
 @Entity
-public class Bunker {
-    @Id
+public class Bunker extends CropEntry{
     @Getter
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private int day;
-    private Date date;
-    private Date time;
     private int thermocoupleOne;
+    @Getter
     private int thermocoupleTwo;
+    @Getter
     private int thermocoupleThree;
+    @Getter
     private float averageThermocouple;
+    @Getter
     private int motorFrequency;
+    @Getter
     private String comment;
 
-    @Getter
-    @ManyToOne
-    @JoinColumn(name = "crop_id")
-    private Crop crop;
-
     public Bunker() {
-
+        this.cropPhase = CropPhase.BUNKER;
     }
-    public Bunker(Crop crop){
-        this.crop = crop;
+    public Bunker(Crop crop, String author,
+                  int thermocoupleOne, int thermocoupleTwo, int thermocoupleThree,
+                  float averageThermocouple, int motorFrequency, String comment) {
+        super(author, null, crop);
+        this.cropPhase = CropPhase.BUNKER;
+        this.thermocoupleOne = thermocoupleOne;
+        this.thermocoupleTwo = thermocoupleTwo;
+        this.thermocoupleThree = thermocoupleThree;
+        this.averageThermocouple = averageThermocouple;
+        this.motorFrequency = motorFrequency;
+        this.comment = comment;
     }
 }
