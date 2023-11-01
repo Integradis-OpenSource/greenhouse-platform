459e6e924afddbf81c8202a906995f9c0b644193
Andres (UPC) <u202116113@upc.edu.pe>
2023-11-01 02:12:10 -0500

    feat(tunnel): added tunnel rest resources and transform.

---
+++ src/main/java/com/integradis/greenhouse/platform/crops/interfaces/rest/transform/TunnelResourceFromEntityAssembler.java
@@ -0,0 +1,10 @@
+package com.integradis.greenhouse.platform.crops.interfaces.rest.transform;
+
+import com.integradis.greenhouse.platform.crops.domain.model.entities.Tunnel;
+import com.integradis.greenhouse.platform.crops.interfaces.rest.resources.TunnelResource;
+
+public class TunnelResourceFromEntityAssembler {
+    public static TunnelResource toResourceFromEntity(Tunnel entity){
+        return new TunnelResource(entity.getId(), entity.getAuthor(), entity.getThermocoupleOne(), entity.getThermocoupleTwo(), entity.getThermocoupleThree(), entity.getAverageThermocouple(), entity.getMotorFrequency(), entity.getRoomTemperature(), entity.getFreshAir(), entity.getRecirculation(), entity.getComment());
+    }
+}
