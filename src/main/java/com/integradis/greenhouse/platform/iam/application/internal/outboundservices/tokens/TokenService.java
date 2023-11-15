0f1a252faf55016cbe67fb6036132fbdf6048545
Nicolas <nespinoza.naer@gmail.com>
2023-11-15 11:58:32 -0500

    feat(sfs): added user details implementation

--- src/main/java/com/integradis/greenhouse/platform/iam/application/internal/outboundservices/tokens/TokenService.java
+++ src/main/java/com/integradis/greenhouse/platform/iam/application/internal/outboundservices/tokens/TokenService.java
@@ -1,7 +1,5 @@
 package com.integradis.greenhouse.platform.iam.application.internal.outboundservices.tokens;
 
-import org.springframework.security.core.Authentication;
-
 public interface TokenService {
     String generateToken(String username);
     String getUsernameFromToken(String token);
