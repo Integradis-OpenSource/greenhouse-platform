9907fb07945409c23b698556ec3cdcf59f657559
CarloLSG <u20211a475@upc.edu.pe>
2023-11-16 13:07:46 -0500

    chore: added class for Cors configuration.

--- src/main/java/com/integradis/greenhouse/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/integradis/greenhouse/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -9,20 +9,9 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class OpenApiConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://greenhouse-open.netlify.app", "*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
+public class OpenApiConfiguration {
     @Bean
     public OpenAPI greenhousePlatformOpenApi() {
         final String securitySchemaName = "bearerAuth";
